8575058c9f9d8d88f7d16299461081c8e4e004fc
Jacob MacDonald <jakemac53@gmail.com>
2018-05-28 10:11:09 -0700

    require a role when starting a white star

--- commands/start.js
+++ commands/start.js
@@ -2,12 +2,27 @@ module.exports = {
 	name: 'start',
 	description: 'Starting a White Star match',
 	args: true,
-	usage: '<opponent>',
+	usage: '@<ws-role> <opponent>',
 	cooldown: 30,
 	execute(message, args) {
-		message.channel.send({ embed: {
-			color: 3447003,
-			description: `Good luck on your White Star! ${args[0]}`,
-		} });
+		if (args.length != 2) {
+			message.reply('Expected exactly two arguments to the "start" command: ' + this.usage);
+			return;
+		}
+		if (message.mentions.roles.size != 1 || !args[0].startsWith('<@')) {
+			message.reply('The first argument to "start" must be a role mention associate this ws with: ' + this.usage);
+			return;
+		}
+		let role = message.mentions.roles.values().next().value;
+		let opponent = args[1];
+
+		// TODO: save this info in the database?
+
+		message.channel.send({
+			embed: {
+				color: 3447003,
+				description: 'Good luck on your White Star against `' + opponent + '` members of <@&' + role.id + '>!\n',
+			}
+		});
 	},
 };
